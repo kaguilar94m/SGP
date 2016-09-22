1e7330910e0c05f9e56a2cb098a73390ed7c240d
devkev95 <devkev95@gmail.com>
2016-09-21 21:57:22 -0600

    Implementación de envio de correo cuando los permisos del usaurio sean asignados Adaptación de la plantilla

--- main/home.php
+++ main/home.php
@@ -13,12 +13,112 @@
 	session_write_close();
 ?>
 <!DOCTYPE html>
-<html>
-<head>
-	<title>Home-Sistema de Generación de Presupuestos y Control de Proyectos</title>
-</head>
-<body>
-	<p><?= $userData->getNombres(), $userData->getApellidos()  ?></p>
-	<p><a href="../services/user/logout.php">Desconectar</a></p>
-</body>
+<html lang="en">
+ <head>
+  <meta charset="utf-8" /><meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <title>Home</title>
+  
+  <!-- Default Styles (DO NOT TOUCH) -->
+  <link rel="stylesheet" href="../lib/CSS/font-awesome.min.css">
+  <link rel="stylesheet" href="../lib/CSS/bootstrap.min.css">
+  <link rel="stylesheet" href="../lib/CSS/fonts.css">
+  <link type="text/css" rel="stylesheet" href="../lib/CSS/soft-admin.css"/>
+  
+  <!-- Adjustable Styles -->
+  
+  <!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
+  <!--[if lt IE 9]>
+   <script src="lib/js/html5shiv.js"></script>
+   <script src="lib/js/respond.min.js"></script>
+  <![endif]-->
+
+ </head>
+ <body>
+ 
+  <div class="cntnr">
+   
+   <!-- RESPONSIVE LEFT SIDEBAR & LOGO -->
+   <div class="left hidden-xs">
+    <div class="logo"><img id="logo" src="../lib/img/logo3.png" style="width:159px !important; height:52px; !important"></div>
+    <div class="sidebar">
+     <div class="accordion">
+      <div class="accordion-group">
+       <div class="accordion-heading">
+        <a class="sbtn btn-default active" href="home.php">
+         <span class="fa fa-home"></span>
+         &nbsp;&nbsp;Home
+        </a>
+       </div>
+      </div>
+      <?php if ($userData->getPerfil() == "Administrador"){ ?>
+      <div class="accordion-group">
+       <div class="accordion-heading">
+        <a class="sbtn btn-default" href="admin/users.php">
+         <span class="fa fa-users"></span>
+         &nbsp;&nbsp;Usuarios
+        </a>
+       </div>
+      </div>
+      <?php } ?>
+      </div>
+      </div>
+      </div>
+   <!-- END LEFT SIDEBAR & LOGO -->
+   
+   <!-- RIGHT NAV, CRUMBS, & CONTENT -->
+   <div class="right">
+   
+    <div class="nav">
+    <div class="bar">
+    	<div class="hov">
+    		<div class="btn-group">
+    		<a class="con" href="" data-toggle="dropdown"><span class="icon icon-user"></span></a>
+    		 <ul class="dropdown-menu pull-right dropdown-profile" role="menu">
+    		 	<li class="title"><span class="icon icon-user"></span>&nbsp;&nbsp;Bienvenido, <?= $userData->getNombres()?></li>
+         		<li><a href="../services/user/logout.php"><span class="fa fa-power-off"></span>Desconectar</a></li>
+    		 </ul>
+    		</div>
+    	</div>
+    </div>
+     
+     <!-- BREADCRUMBS -->
+     <div class="crumbs">
+      <ol class="breadcrumb hidden-xs">
+       <li class="active"><i class="fa fa-home"></i> Home</li>
+      </ol>
+     </div>
+    </div>
+    
+    <!-- BEGIN PAGE CONTENT -->
+    <div class="content">
+     <div class="page-h1">
+      <h1>Bienvenido <strong><?php echo $userData->getNombres()." ".$userData->getApellidos() ?></strong> ha ingresado al sistema como <strong>Administrador</strong></h1>
+     </div>
+     <div class="tbl">
+     	<div class="col-md-11">
+     	<h4>A la izquiera puede encontrar un menú con todas las acciones que puede realizar en el sistema</h4>
+	    </div>
+     </div>
+
+     
+       
+    </div>
+    <!-- END PAGE CONTENT -->
+
+   </div>
+   <!-- END NAV, CRUMBS, & CONTENT -->
+   
+  </div>
+  
+  <!-- Default JS (DO NOT TOUCH) -->
+  <script src="../lib/JS/jquery-3.1.0.min.js"></script>
+  <script src="../lib/JS/bootstrap.min.js"></script>
+  <script src="../lib/JS/hogan.min.js"></script>
+  <script src="../lib/JS/typeahead.min.js"></script>
+  <script src="../lib/JS/typeahead-example.js"></script>
+  
+  <!-- Adjustable JS -->
+  <script src="../lib/JS/soft-widgets.js"></script>
+  
+ </body>
 </html>
